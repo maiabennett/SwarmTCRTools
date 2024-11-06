b3fb08375702e8311f18ad4dbf17a97ef73c2b67
maiabennett <123126475+maiabennett@users.noreply.github.com>
2024-11-06 17:12:05 -0500

    Added SwarmTCR results, results processing

--- util/plotting.R
+++ util/plotting.R
@@ -2,13 +2,13 @@
 #########
 # Plots #
 #########
-plotRefWeights <- function(all.weights, set.weights, directory) {
+plotRefWeights <- function(all.weights, set.weights) {
   all.long <- all.weights %>% 
     pivot_longer(cols = starts_with("CDR"), names_to = "CDR", values_to = "Weight")
   set.long <- set.weights %>% 
     pivot_longer(cols = starts_with("CDR"), names_to = "CDR", values_to = "Weight")
   epitopes <- unique(all.long$Epitope)
-  ggplot(all.long, aes(x = CDR, y = Weight, color = Epitope)) +
+  plot <- ggplot(all.long, aes(x = CDR, y = Weight, color = Epitope)) +
     geom_boxplot() +
     geom_point(data =set.long, aes(x = CDR, y = Weight), color = "black") +
     facet_wrap(~Epitope) +
@@ -17,83 +17,106 @@ plotRefWeights <- function(all.weights, set.weights, directory) {
     labs(title = "Reference Weights",
          x = "CDR",
          y = "Weight")
+
+  return(plot)
 }
 
-plotScoreThresholds <- function(pr.dfs, scores.dfs, epitopes, thresholds, directory) {
+plotPrecisionRecall <- function(pr.dfs, epitopes, directory = NULL, thresholds = NULL) {
+
   pr.long <- data.frame()
-  scores.long <- data.frame()
   for (epitope in epitopes) {
     pr <- pr.dfs[[epitope]]
     pr <- pr %>% 
       mutate(Epitope = epitope)
+    pr.long <- rbind(pr.long, pr)
+  }
+
+  average.pr <- pr.long %>%
+    group_by(Epitope, Recall) %>%
+    summarize(Precision = mean(Precision)) %>%
+    ungroup()
+
+  pr.auc = data.frame()
+  for (epitope in epitopes) {
+    pr <- pr.long %>% filter(Epitope == epitope)
+    ord <- order(pr$Recall)
+    recall <- pr$Recall[ord]
+    precision <- pr$Precision[ord]
+    pr.auc <- rbind(pr.auc, data.frame(
+        Epitope = epitope, 
+        PR_AUC = sum(diff(recall) * (precision[-length(precision)] + precision[-1]) / 2)))
+  }
+
+  if (!is.null(directory)) {
+    write.csv(pr.auc, paste0(directory, "pr-auc.csv"), row.names = FALSE)
+  }
+
+  average.pr <- average.pr %>% 
+    mutate(Epitope = factor(Epitope, levels = unique(average.pr$Epitope), labels = paste0(unique(average.pr$Epitope), "\nPR AUC: ", pr.auc$PR_AUC %>% round(3))))
+
+  plot <- ggplot(average.pr, aes(x = Recall, y = Precision, color = Epitope)) +
+    geom_smooth(method = "loess", se = TRUE) +
+    labs(title = "Average Precision-Recall Curve", x = "Recall", y = "Precision") +
+    facet_wrap(~Epitope) +
+    theme_minimal() +
+    theme(legend.position = "none") 
+
+  if (!is.null(thresholds)) {
+    thresholds <- thresholds %>% 
+      left_join(pr.auc, by = "Epitope") %>% 
+      mutate(Epitope = paste0(Epitope, "\nPR AUC: ", PR_AUC %>% round(3))) %>% 
+      mutate(label = paste0("F1: ", F1 %>% round(3), "\nFbeta: ", Fbeta %>% round(3), "\nPrecision: ", Precision %>% round(3), "\nRecall: ", Recall %>% round(3), "\nThreshold: ", Threshold %>% round(3)))
+    plot <- plot +
+      geom_point(data = thresholds, aes(x = Recall, y = Precision), color = "black", size = 5) +
+      geom_text(data = thresholds, aes(x = Recall, y = Precision, label = label), color = "black", hjust = 0, vjust = 0)
+  }
+
+  return(plot)
+}
+
+plotScores <- function(scores.dfs, epitopes, thresholds = NULL, fill = "Flag", labels = TRUE) {
+
+  scores.long <- data.frame()
+  for (epitope in epitopes) {
     scores <- scores.dfs[[epitope]]
     scores <- scores %>% 
       mutate(Epitope = epitope)
-    pr.long <- rbind(pr.long, pr)
     scores.long <- rbind(scores.long, scores)
   }
 
-  (plot <- plotPrecisionRecall(pr.long, thresholds))
-  print(plot)
-  ggsave(paste0(directory, "thresholdPRs.png"), plot, width = 20, height = 10)
-
-  (plot <- plotScores(scores.long, thresholds))
-  print(plot)
-  ggsave(paste0(directory, "scoreDistributions.png"), plot, width = 20, height = 10)
-
-}
-
-plotPrecisionRecall <- function(pr, thresholds) {
-  labels <- c()
-  for (epitope in unique(thresholds$Epitope)) {
-    labels <- c(labels, paste0(epitope, 
-      "\nF1: ", thresholds %>% filter(Epitope == epitope) %>% pull(F1) %>% round(3),
-      #"\nPrecision: ", thresholds %>% filter(Epitope == epitope) %>% pull(Precision) %>% round(3),
-      #"\nRecall: ", thresholds %>% filter(Epitope == epitope) %>% pull(Recall) %>% round(3),
-      "\nThreshold: ", thresholds %>% filter(Epitope == epitope) %>% pull(Threshold) %>% round(3)))
-  }
-  pr <- pr %>% 
-    mutate(Epitope = factor(Epitope, levels = unique(pr$Epitope), labels = labels))
-  thresholds <- thresholds %>% 
-    mutate(Epitope = factor(Epitope, levels = unique(thresholds$Epitope), labels = labels))
-  ggplot(pr, aes(x = Recall, y = Precision, color = as.factor(Run))) +
-    geom_line() +
-    facet_wrap(~Epitope) +
-    coord_fixed(ratio = 1) +
-    scale_color_viridis(discrete = TRUE, option = "F", begin = .3, end = .7) +
-    scale_x_continuous(limits = c(0, 1)) +
-    scale_y_continuous(limits = c(0, 1)) +
-    geom_point(data = thresholds, aes(x = Recall, y = Precision), color = "black", size = 5) +
-    theme(legend.position = "none") 
-}
-
-plotScores <- function(scores, thresholds, fill = "Flag", labels = TRUE) {
-  thresholds <- thresholds %>%
-      filter(Epitope %in% unique(scores$Epitope))
-  scores <- scores %>%
-    na.omit()
-  if (labels) {
-    labels <- c()
-    order <- unique(scores$Epitope)
-    for (epitope in order) {
-      labels <- c(labels, paste0(epitope, 
-        "\nF1: ", thresholds %>% filter(Epitope == epitope) %>% pull(F1) %>% round(3),
-        #"\nPrecision: ", thresholds %>% filter(Epitope == epitope) %>% pull(Precision) %>% round(3),
-        #"\nRecall: ", thresholds %>% filter(Epitope == epitope) %>% pull(Recall) %>% round(3),
-        "\nThreshold: ", thresholds %>% filter(Epitope == epitope) %>% pull(Threshold) %>% round(3)))
-    } 
-    scores <- scores %>% 
-      mutate(Epitope = factor(Epitope, levels = order, labels = labels))
+  
+  if (!is.null(thresholds)) {
     thresholds <- thresholds %>%
-      mutate(Epitope = factor(Epitope, levels = order, labels = labels))
-  }
-  ggplot(scores, aes(x = Score, fill = as.factor(!!sym(fill)))) +
+        filter(Epitope %in% unique(scores.long$Epitope))
+    scores.long <- scores.long %>%
+      na.omit()
+    if (labels) {
+      labels <- c()
+      order <- unique(scores.long$Epitope)
+      for (epitope in order) {
+        labels <- c(labels, paste0(epitope, 
+          "\nThreshold: ", thresholds %>% filter(Epitope == epitope) %>% pull(Threshold) %>% round(3)))
+      } 
+      scores.long <- scores.long %>% 
+        mutate(Epitope = factor(Epitope, levels = order, labels = labels))
+      thresholds <- thresholds %>%
+        mutate(Epitope = factor(Epitope, levels = order, labels = labels))
+    }
+
+  plot <- ggplot(scores.long, aes(x = Score, fill = as.factor(!!sym(fill)))) +
     geom_density(alpha = 0.5) +
-    geom_vline(data = thresholds, aes(xintercept = Threshold, linetype = "dashed"), color = "black") +
-    facet_wrap(~Epitope) +
-    theme(legend.position = "none") +
+    facet_wrap(~Epitope, scales = "free") +
+    # theme(legend.position = "none") +
     scale_fill_viridis(discrete = TRUE, option = "F", begin = .3, end = .7) +
     labs(title = "Score Distributions",
          x = "Score",
          y = "Density")
+
+    if (!is.null(thresholds)) {
+    plot <- plot +
+      geom_vline(data = thresholds, aes(xintercept = Threshold, linetype = "dashed"), color = "black")
+    }
+  }
+
+  return(plot)
 }
